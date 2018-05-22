ed27fe136a19b7de7563cbb4bf8b9bf5bd2e8126
limdingdong <goldbat9554@naver.com>
2018-05-22 15:42:17 +0900

    3장 종료

---
+++ src/main/java/com/nagong/webservice/web/WebRestController.java
@@ -0,0 +1,26 @@
+package com.nagong.webservice.web;
+
+import com.nagong.webservice.dto.posts.PostsSaveRequestDto;
+import com.nagong.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+
+    @GetMapping("/hello")
+    public String hello () {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
+    }
+}
--- src/test/java/com/nagong/webservice/ApplicationTests.java
+++ src/test/java/com/nagong/webservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nagong.pond;
+package com.nagong.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Application {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
